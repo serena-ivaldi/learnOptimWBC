835fdffcb7f281fe4b96d0cc58e78f7f6f4f1d73
Serena <serena.ivaldi@gmail.com>
2015-02-13 11:29:43 +0100

    batch 7

---
+++ matlab/test/results/info_7_sere/AllRuntimeParameters.m
@@ -0,0 +1,57 @@
+
+%%%;;
+
+%% GENERAL PARAMETERS
+% for other strucutures
+time_struct.ti = 0;
+time_struct.tf = 10;
+time_struct.step = 0.1;
+
+%% for simulation 
+time_sym_struct = time_struct;
+time_sym_struct.step = 0.001; 
+% define the type of integration of the sytem of differential equation
+fixed_step = false;
+
+%% TASK PARAMETERS
+name_dat = 'sere/LBR4p5.0_scene5_UF_repellers_on_elbow__atrtactive_point_on_ee_fit5_SERE';
+%name_dat = 'LBR4p5.0_scene5_UF_repellers_on_elbow__atrtactive_point_on_ee_fit5';
+%name_dat = 'LBR4p5.0__scene5_repellers_on_elbow__atrtactive_point_on_ee_fit5';
+path=LoadParameters(name_dat);
+load(path);
+
+%% SCENARIO
+name_scenario = 'lbr_scenario5.1';
+
+%% STARTING CONDITION FOR SIMULATION
+% TODO generalize for multichain
+%qi{1} = qz;
+qi{1} = zeros(1,chains.GetNumLinks(1));
+qdi{1} = zeros(1,chains.GetNumLinks(1));
+options= [];
+simulator_type = {'rbt'};
+
+%% Parameters Dependant on the type of controller
+
+%%%EOF
+
+switch CONTROLLERTYPE
+    case 'UF'
+        UF_RuntimeParameters
+    case 'GHC'
+        GHC_RuntimeParameters
+    otherwise
+        warning('Unexpected control method')
+end
+
+
+%% DO NOT CHANGE THIS PART!
+
+
+% backup data 
+rawTextFromStorage = fileread(which(mfilename));
+rawTextFromStorage = regexp(rawTextFromStorage,['%%%;;' '(.*?)%%%EOF'],'match','once');
+
+% join the general static parameter with the particular static one
+rawTextFromStorage = strcat(rawTextFromStorage,rawTextFromStoragePart);
+
