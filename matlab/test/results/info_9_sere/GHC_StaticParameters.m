4e9e98cbe3c274dac593c4b90021b26689c50961
Serena <serena.ivaldi@gmail.com>
2015-02-17 19:41:26 +0100

    batch of experiments

---
+++ matlab/test/results/info_9_sere/GHC_StaticParameters.m
@@ -0,0 +1,42 @@
+disp('GHC_STATICPARAM')
+
+%%%;;
+
+%% chained alpha 
+matrix1 = [0 1 0;0 0 0;1 1 0];  % 2>1>3
+matrix2 = [0 0 0;1 0 0;1 1 0];  % 1>2>3
+matrix3 = [0 0 0;1 0 1;1 0 0];  % 1>3>2
+matrix4 = [0 0 1;1 0 1;0 0 0];  % 3>1>2
+matrix_value(:,:,1) = matrix1;
+matrix_value(:,:,2) = matrix2;
+matrix_value(:,:,3) = matrix3;
+matrix_value(:,:,4) = matrix4;
+ti =[2 6 8];
+
+
+%% CONTROLLER PARAMETERS
+% % row vector one for each chain
+kp = [1000 1000 1000]; 
+kd = [2*sqrt(kp) 2*sqrt(kp) 2*sqrt(kp)];
+
+for i= 1:chains.GetNumChains()
+  
+   for par = 1:chains.GetNumTasks(i)
+       K_p = kp(i,par)*eye(size(dim_of_task{i,par},1));  
+       K_d = kd(i,par)*eye(size(dim_of_task{i,par},1)); 
+       Kp{i,par} = K_p;
+       Kd{i,par} = K_d;
+       
+   end
+   
+end
+%---
+
+
+%%%EOF
+
+
+%% DO NOT MODIFY THIS PART 
+
+rawTextFromStoragePart = fileread(which(mfilename));
+rawTextFromStoragePart = regexp(rawTextFromStoragePart,['%%%;;' '(.*?)%%%EOF'],'match','once');
---
+++ matlab/test/results/info_9_sere/UF_StaticParameters.m
@@ -0,0 +1,46 @@
+disp('UF_STATICPARAM')
+
+%%%;;
+
+% REPELLER PARAMETERS
+% scenario dependant
+rep_subchain = [3];
+rep_target_link{1} = rep_subchain;
+rep_type = {'cartesian_x'};
+rep_mask {1,1}=[1,1,1]; 
+rep_type_of_J_rep = {'DirectionCartesian'};
+for ii=1:chains.GetNumChains()
+    chain_dof(ii) = chains.GetNumLinks(ii);
+end
+
+
+
+%CONTROLLER PARAMETERS
+% the metric change between regularized and not regularized because in the
+% regularized case i have to do N^(-1) 
+% not regularized case i have N^(-1/2)
+metric = {'M'};  % ex: if N = M^(-1) so N^(-1/2) = (M^(-1))^(-1/2) = M^(1/2);        
+
+
+kp = [700]; % row vector one for each chain
+kd = [2*sqrt(kp)];
+
+for i= 1:chains.GetNumChains()
+   for par = 1:chains.GetNumTasks(i)
+       K_p = kp(i,par)*eye(size(dim_of_task{i,par},1));  
+       K_d = kd(i,par)*eye(size(dim_of_task{i,par},1)); 
+       Kp{i,par} = K_p;
+       Kd{i,par} = K_d;
+   end
+   
+end
+
+
+
+%%%EOF
+
+
+%% DO NOT MODIFY THIS PART 
+
+rawTextFromStoragePart = fileread(which(mfilename));
+rawTextFromStoragePart = regexp(rawTextFromStoragePart,['%%%;;' '(.*?)%%%EOF'],'match','once');
