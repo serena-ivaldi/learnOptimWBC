1d5e7578a67f1b84af73ed50afd8f4223ad5adb5
vale <valerio.modugno@gmail.com>
2015-08-17 21:52:14 +0200

    added fitness10

---
+++ matlab/classes/@Instance/fitness10.m
@@ -0,0 +1,79 @@
+
+
+function fit = fitness10(obj,t,q)
+    global G_OB;
+    
+    %%%;;
+    downsaple = 10;
+    L = 1; 
+    max_effort = 1.6589e+07;
+    max_traj_error = 5000;
+    weight_effort = 1;
+    weight_traj_err = 1;
+    hitting_condition = 0.05;
+    hitting = false;
+    joint_violation = false; % joint in deg
+    %%%EOF
+    contr = obj.controller;
+    traj_err= 0;
+    
+
+    for i=1:downsaple:size(t,2)
+        q_cur = q{1}(i,:);
+        % joint limits in deg for the second joint of jaco[47 313]
+        if(q_cur(2)*(180/pi)<47 || q_cur(2)*(180/pi)>313)
+           joint_violation = true;
+        end
+        % joint limits in deg for the third joint of jaco[47 313] [19 341]
+        if(q_cur(2)*(180/pi)<47 || q_cur(2)*(180/pi)>341)
+           joint_violation = true;
+        end
+        % compute the trajectory error (absolute error)
+        kinematic=CStrCatStr({'contr.subchains.sub_chains{1}.T0_'},num2str(contr.subchains.GetNumSubLinks(1,1)),{'(q_cur)'});
+        T = eval(kinematic{1});
+        ee = T(1:3,4);
+        attr_pos = contr.references.GetTraj(1,1,t(i)); 
+        traj_err = traj_err + norm((ee - attr_pos),L);
+        % verify the pruning conditions
+        for jj=1:size(G_OB,2)
+            dist = G_OB(jj).Dist(ee',L);
+            if(dist < hitting_condition)
+               hitting = true;
+            end
+        end
+        if(hitting || joint_violation)
+           break;
+        end
+    end
+    % max effort
+    effort = sum((contr.torques{1}(:).*contr.torques{1}(:)),2);
+    effort = sum(effort,1);
+    
+    
+    % saturations 
+    if(effort>max_effort)
+       effort = max_effort;
+    end
+    
+    if(traj_err>max_traj_error)
+       traj_err = max_traj_error;
+    end
+    
+    traj_err  = traj_err/max_traj_error;
+    effort = effort/max_effort;
+    
+    %%DEBUG
+    fprintf('traj error is %f\n', traj_err)
+    fprintf('effort term is %f\n', effort)
+    %---
+    fit = (-traj_err*(weight_traj_err) - effort*(weight_effort))*( 1/(weight_traj_err+weight_effort) );
+    
+    if(hitting)
+      fit = -1;
+      disp('hit the obstacle')
+    end
+    if(joint_violation)
+      fit = -1;
+      disp('joint violation')
+    end
+end
