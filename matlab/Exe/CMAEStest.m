6111d5a9a3365fbfd1709354671d4b011e201d81
vale <valerio.modugno@gmail.com>
2015-10-30 19:40:22 +0100

    updated cmaes test for benchmarking

--- matlab/Exe/CMAEStest.m
+++ matlab/Exe/CMAEStest.m
@@ -3,27 +3,26 @@ clear variables
 close all
 clc
 
-
-
-function_2_test = 'g06';
-
-method_to_use = 'adaptive';  % adaptive , vanilla
-
-
+%% DATA 1
+function_2_test = {'g06'};
+method_to_use = {'adaptive','vanilla'};  % adaptive , vanilla
 search_space_dimension = 2;
+repetition_of_the_experiment = 100;
+
  %% CONSTRAINTS PARAMETERS
- if(strcmp(function_2_test,'g06'))
+ if(strcmp(function_2_test{1},'g06'))
      epsilon = [1, 1, 1];
      constraints_functions = {'g06Constr1','g06Constr2','g06Constr3'}; 
      constraints_type = [1 1 1];      
      constraints_values =[0,0,0];
+     n_constraints = length(constraints_functions);
  end
  
  activate_constraints_handling = true;
 
  %% INSTANCE PARAMETER
  run_function = @EmptyPreprocessing;
- if(strcmp(function_2_test,'g06'))
+ if(strcmp(function_2_test{1},'g06'))
     fitness = @g06;
  end
  clean_function = @EmptyPostprocessing;
@@ -31,7 +30,7 @@ search_space_dimension = 2;
  %% CMAES PARAMETER
  explorationRate = 0.1; %0.1; %0.5; %0.1;%[0, 1]
  niter = 1000;  %number of generations
- if(strcmp(function_2_test,'g06'))
+ if(strcmp(function_2_test{1},'g06'))
     cmaes_value_range = [0 , 100];  % boudn that define the search space
  end
  % starting value of parameters
@@ -46,18 +45,74 @@ search_space_dimension = 2;
         
  end
  
- %% Constraints
- if(strcmp(method_to_use,'vanilla'))
-    constr=Optimization.FixPenalty(search_space_dimension,constraints_functions,constraints_type,constraints_values);
- elseif(strcmp(method_to_use,'adaptive'))
-    constr = Optimization.AdaptivePenalty(epsilon,niter,search_space_dimension,constraints_functions,constraints_type,constraints_values);
- end
-     
- %% Instance
- inst =  Optimization.Instance(constr,activate_constraints_handling,run_function,fitness,clean_function,[]);
-
+ %% CONSTRAINTS
  
- tic
- [mean_performances, bestAction, BestActionPerEachGen, policies, costs, succeeded] = inst.CMAES(search_space_dimension,start_action,niter,explorationRate,cmaes_value_range);
- exec_time = toc
+ for i=1:length(method_to_use) 
+     if(strcmp(method_to_use{i},'vanilla'))
+        constr{i}=Optimization.FixPenalty(search_space_dimension,constraints_functions,constraints_type,constraints_values);
+     elseif(strcmp(method_to_use{i},'adaptive'))
+        constr{i} = Optimization.AdaptivePenalty(epsilon,niter,search_space_dimension,constraints_functions,constraints_type,constraints_values);
+     end
+ end
+ %% DATA 2
+ lambda = round(4 + 3 * log(search_space_dimension)); % number of candidates used in cmaes
+ last_generation = 100; % look to the last n = last_generation to look for the best (hopefully in steady state)
+ 
+ 
+ all_perfomance = zeros(repetition_of_the_experiment,niter*lambda);
+ all_best = zeros(repetition_of_the_experiment,search_space_dimension);
+ all_last_best = zeros(repetition_of_the_experiment,search_space_dimension);
+ all_best_violations = zeros(repetition_of_the_experiment,n_constraints);
+ all_last_best_violations = zeros(repetition_of_the_experiment,n_constraints);
+ 
+ %% OPTIMIZATION
+ 
 
+ for jj=1:length(method_to_use)
+     for kk = 1:repetition_of_the_experiment
+         inst =  Optimization.Instance(constr{jj},activate_constraints_handling,run_function,fitness,clean_function,[]);
+         tic
+         [mean_performances, bestAction, BestActionPerEachGen, policies, costs, succeeded] = inst.CMAES(search_space_dimension,start_action,niter,explorationRate,cmaes_value_range);
+         exec_time = toc
+         all_perfomance(kk,:) = -costs;
+         all_best(kk,1) = bestAction.performance;
+         [tmp , id]=min(costs(end-last_generation:end));
+         all_last_best(kk,1) = -costs(id);
+         
+         for ii=1:n_constraints
+             all_best_violations(kk,ii) = feval(constraints_functions{ii},bestAction.parameters);
+             all_last_best_violations(kk,ii) = feval(constraints_functions{ii},policies(id,:));
+         end
+     end
+     
+     % compute the mean and variance of overall perfomance
+     prf.average  = mean(all_perfomance,2); 
+     prf.variance = var(all_perfomance,2);
+     
+     
+     
+     all_prf{jj} = prf;
+     % mean and variance of the best perfomance over all the experiments
+     G_best(jj,1) = mean(all_best,2);
+     G_best(jj,2) = var(all_best,2);
+     % mean and variance of the best perfomance of the last k generations
+     % over all the experiments
+     G_last_best(jj,1) = mean(all_last_best,2);
+     G_last_best(jj,2) = var(all_last_best,2);
+     
+      for ii=1:n_constraints
+         G_best_violations.mean(1,ii) = mean(all_best_violations(:,ii),2); 
+         G_best_violations.variance(1,ii) = mean(all_best_violations(:,ii),2); 
+         G_last_best_violations.mean(1,ii) = mean(all_last_best_violations(:,ii),2); 
+         G_last_best_violations.variance(1,ii) = mean(all_last_best_violations(:,ii),2); 
+      end
+     
+      
+      
+     
+     
+     
+     
+     
+ end
+ 
