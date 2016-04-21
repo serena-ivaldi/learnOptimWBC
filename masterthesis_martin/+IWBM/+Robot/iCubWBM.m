521a27edff6d9b89cf1a28cc5ca85d113e7260f4
Martin Neururer <martin.neururer@gmail.com>
2016-04-21 17:55:54 +0200

    Replaced the intialization of the WBM with an internal init-function.

--- masterthesis_martin/+IWBM/+Robot/iCubWBM.m
+++ masterthesis_martin/+IWBM/+Robot/iCubWBM.m
@@ -22,44 +22,11 @@ classdef iCubWBM < IWBM.WBMInterface
     methods
         % Constructor:
         function obj = iCubWBM(robot_model, robot_config, wf2FixLnk, L, varargin)
-            if ( ~exist('robot_model', 'var') && ~exist('robot_config', 'var') )
-                % base model parameters:
-                robot_model = WBM.wbmBaseModelParams;
-                robot_model.urdfRobot    = 'icubGazeboSim';
-                robot_model.wf_R_rootLnk = eye(3,3);
-                robot_model.wf_p_rootLnk = zeros(3,1);
-                robot_model.g_wf         = [0; 0; -9.81];
-
-                % base robot config:
-                robot_config = WBM.wbmHumanoidConfig;
-                robot_config.ndof          = 25;
-                robot_config.nCstrs        = 2;
-                robot_config.cstrLinkNames = {'l_sole', 'r_sole'};
-                robot_config.dampCoeff     = 0.00; %0.75;
-                % setup the body of the iCub-Robot with the initial body (joint) positions (in degrees):
-                % note: this configuration assumes an iCub-Robot with 25 DoFs.
-                robot_config.body           = WBM.Robot.iCub.setupBody_iCub();
-                robot_config.jpos_torso     = [-10.0; 0.0; 0.0];
-                robot_config.jpos_left_arm  = [-19.7; 29.7; 0.0; 44.9; 0.0];
-                robot_config.jpos_left_leg  = [25.5; 0.1; 0.0; -38.5; -5.5; -0.1];
-                robot_config.jpos_right_arm = robot_config.pos_leftArm;
-                robot_config.jpos_right_leg = robot_config.pos_leftLeg;
-                % init-state parameters:
-                robot_config.initStateParams.x_b     = zeros(3,1);
-                robot_config.initStateParams.qt_b    = zeros(4,1);
-                robot_config.initStateParams.q_j     = [robot_config.jpos_torso; robot_config.jpos_left_arm; robot_config.jpos_right_arm; ...
-                                                        robot_config.jpos_left_leg; robot_config.jpos_right_leg] * (pi/180.0); % in radians
-                robot_config.initStateParams.dx_b    = zeros(3,1);
-                robot_config.initStateParams.omega_b = zeros(3,1);
-                robot_config.initStateParams.dq_j    = zeros(robot_config.ndof,1);
-            end
-            
             if ~exist('wf2FixLnk', 'var') )
                 wf2FixLnk = true;
             end
-
             % init the mex-WholeBodyModel for the iCub-Robot:
-            mwbm_robot = WBM(robot_model, robot_config, wf2FixLnk);
+            mwbm_robot = WBM.Robot.iCub.initRobot_iCub(wf2FixLnk);
         end
 
         function delete(obj)
@@ -83,7 +50,7 @@ classdef iCubWBM < IWBM.WBMInterface
 
         end
 
-        function J0 = jacob0(obj, q_j, varargin)            
+        function J0 = jacob0(obj, q_j, varargin)
             J0 = obj.mwbm_robot.jacobian(obj.mwf_R_b, obj.mwf_p_b, q_j);
         end
 
